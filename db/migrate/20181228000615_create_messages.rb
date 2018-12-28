bc6b334a5b38815ed0ee941d5de5b8e3b45c6161
Adrian Messado <adrian.messado@gmail.com>
2018-12-27 16:24:23 -0800

    add additional comment

--- db/migrate/20181228000615_create_messages.rb
+++ db/migrate/20181228000615_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       # t.text :content will create a text column called content in the messages table.
       t.text :content
+      # t.timestamps is a Rails command that creates two more columns in the messages table called created_at and updated_at. These columns are automatically set when a message is created or updated
       t.timestamps
     end
   end
