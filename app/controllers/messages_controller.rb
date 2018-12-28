f3529946546fedc0fd34e2613d498f7b89cbca4d
Adrian Messado <adrian.messado@gmail.com>
2018-12-27 16:36:08 -0800

    add comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+	# add an index action to messages_controller that retrieves all messages from the database and stores them in @messages
 	def index
 		@messages = Message.all
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # messages#index displays a list of all messages
   get 'messages' => 'messages#index'
 end
