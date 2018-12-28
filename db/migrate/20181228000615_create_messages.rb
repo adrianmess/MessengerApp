2bdc88fb1ca33a938af7efc5ac3114870cfde711
Adrian Messado <adrian.messado@gmail.com>
2018-12-27 16:18:17 -0800

    add comment

--- db/migrate/20181228000615_create_messages.rb
+++ db/migrate/20181228000615_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
+      # t.text :content will create a text column called content in the messages table.
       t.text :content
       t.timestamps
     end
