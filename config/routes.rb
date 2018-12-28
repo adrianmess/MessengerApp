02a2bb5c1311abeb41e23e79a899eaf3868d8b55
Adrian Messado <adrian.messado@gmail.com>
2018-12-27 19:27:49 -0800

    create route that maps requests to messages/new to the Message controller's new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # messages#index displays a list of all messages
   get 'messages' => 'messages#index'
+  get 'messages/new' => 'messages#new'
 end
